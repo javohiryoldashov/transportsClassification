ced507c82bcac36ef6521650ff041ab79e7ad25c
javohiryoldashov <108182233+javohiryoldashov@users.noreply.github.com>
2023-11-13 16:51:34 +0500

    'project'

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+#title
+st.title('Three type of vhicl classification')
+
+file = st.file_uploader('Rasm yuklash', type=['png', 'jpg', 'jpeg', 'gif', 'svg'])
+if file:
+    st.image(file)
+    img = PILImage.create(file)
+    model = load_learner('transport_model.pkl')
+
+    pred, pred_id, probs = model.predict(img)
+
+    st.success(pred)
+    st.info(f'Extimolik: {probs[pred_id]*100:.1f}')
+
+    fig = px.bar(x = probs*100, y = model.dls.vocab)
+    st.plotly_chart(fig)
